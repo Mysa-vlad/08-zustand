86460fec5114b588cfb0cfe245e1fff53b62eeb2
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-06-15 23:02:37 +0300

    second try

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -21,11 +21,11 @@ export default function NotesClient({ tag }: NotesClientProps) {
 
 
 
-  // const [prevTag, setPrevTag] = useState<string>(tag)
-  // if (tag !== prevTag) {
-  //   setPage(1)
-  //   setPrevTag(tag)
-  // }
+  const [prevTag, setPrevTag] = useState<string>(tag)
+  if (tag !== prevTag) {
+    setPage(1)
+    setPrevTag(tag)
+  }
 
 
   useEffect(() => {
